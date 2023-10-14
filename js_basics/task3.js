b5b505b82507ab0cb0526f24ee108e6e7147b3ef
unknown <elena.zhorova@icloud.com>
2023-10-14 16:13:56 +0300

    New branch feat-js-basics with changes according to the comments

--- js_basics/task3.js
+++ js_basics/task3.js
@@ -1,8 +1,11 @@
 // Конкатенація радків та шаблонний рядок
 const name1 = "Viki"
 const name2 = "Olena"
+const meetingMessage = name1 + " " + name2 + " " + "nice to meet you again!"
 const names = `${name1} and ${name2} welcome!`
+console.log(meetingMessage)
 console.log(names)
+
 
 var nameBaby= "Viki"
 var nameAdult= "Olena"
