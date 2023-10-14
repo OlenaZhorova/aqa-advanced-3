6d32e309ababde8b936f1b7dd8fbe76c9e948dbd
Olena Zhorova <146441718+OlenaZhorova@users.noreply.github.com>
2023-10-14 03:23:52 +0300

    Delete js_basics_test directory

--- js_basics_test/operators2.js
+++
@@ -1,7 +0,0 @@
-const isSunny = true
-const isWarm = false
-
-// console.log(isSunny && isWarm)
-// console.log(isSunny || isWarm)
-
-console.log(isSunny || !isWarm)
--- js_basics_test/variables.js
+++
@@ -1,8 +0,0 @@
-const AGE = 12
-
-// naming
-const personeAge = 23
-const streetName = "Central"
-const ADULT_AGE =18
-
-
