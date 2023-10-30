a08dac1713a32b5f5d4e0201a5a7df557c7af121
unknown <elena.zhorova@icloud.com>
2023-10-30 16:26:11 +0200

    Add a new branch feat-js-objects with changes, according to comments

--- JS_Objects/Task1.js
+++ JS_Objects/Task1.js
@@ -1,14 +1,12 @@
 // Завдання 1: Робота з об'єктом
-const student = {
-    name: {
-        firstName: "Alice",
-        lastName: "Gromm"
-    },
-    age: 25,
-    courses: {
+const student = {}
+    student.firstName= "Alice"
+    student.lastName= "Gromm"
+    student.age= 25
+    student.course = {
         course1: "Math",
         course2: "History",
         course3: "Computer"
     }
-}
+
 console.log(student)
