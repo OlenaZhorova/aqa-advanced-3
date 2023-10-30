e00824f07c1972630e1ece8b4d7ba33984b85655
unknown <elena.zhorova@icloud.com>
2023-10-31 00:42:55 +0200

    New branch feat-js-arrays with changes, according to the comments with homework 5

--- Arrays/Task1.js
+++ Arrays/Task1.js
@@ -1,17 +1,26 @@
 // Завдання 1: Відстеження кількості позитивних,
 // негативних та нульових чисел
-const numbers = [1, -1, 0, 2, -2, 5, -5, 9, 0]
 
-for (let positiveCount of numbers) {
-    if (positiveCount > 0)
-        console.log('Позитивне число: ', positiveCount + 1)
+//     Створіть масив чисел за допомогою конструкції const numbers = [2, -5, 0, 7, -3, 0, 10, -8].
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8]
+//     Задайте змінні positiveCount, negativeCount та zeroCount зі значенням 0. Вони будуть використовуватись для відстеження кількості позитивних, негативних та нульових чисел в масиві.
+let positiveCount = 0
+let negativeCount = 0
+let zeroCount = 0
+//     Використовуючи цикл for, пройдіться по кожному елементу масиву numbers.
+//     Перевірте чи число позитивне, негативне чи нульове, та збільште відповідну змінну (positiveCount, negativeCount або zeroCount) на 1.
+for (const number of numbers) {
+    if(number === 0){
+        zeroCount++
+    }else if (number > 0) {
+        positiveCount++
+    }else if (number < 0){
+        negativeCount++
+    }else {
+        console.log(`${number} is not a number`)
+    }
 }
-for (let negativeCount  of numbers) {
-    if (negativeCount < 0 )
-        console.log('Негативне число: ', negativeCount)
-}
-for (let zeroCount  of numbers) {
-    if (zeroCount === 0)
-        console.log('Нульове число: ', zeroCount)
-}
-
+// Після завершення циклу виведіть значення змінних positiveCount, negativeCount та zeroCount на консоль.
+console.log("zeroCount: ", zeroCount)
+console.log("positiveCount: ", positiveCount)
+console.log("negativeCount: ", negativeCount)
--- Arrays/Task2.js
+++ Arrays/Task2.js
@@ -1,6 +1,5 @@
 // Завдання 2: Копіювання з мутацією
 
 const numbers = [1, 2, 3, 4, 5]
-const index = numbers.length - 1
 const newArray = numbers.map((num, index) => num * index)
 console.log(newArray)
