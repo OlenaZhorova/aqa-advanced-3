177bdc283b801729340fe4434d5b5ca683959e15
unknown <elena.zhorova@icloud.com>
2023-11-07 00:15:40 +0200

    New branch feat-js-promisses with homework8 promises

---
+++ promises/Task2.js
@@ -0,0 +1,54 @@
+// Завдання 2
+// Напишіть функцію яке буде робити запит до серверу за цим ендпоінтом
+// https://jsonplaceholder.typicode.com/todos/1
+// Функція повинна повертати як результат Promise що повертає об’єкт todo
+
+const url1 = "https://jsonplaceholder.typicode.com/todos/1"
+function fetchToDo(url1) {
+    return fetch(url1)
+        .then((Response) => Response.json())
+        .then((jsonBody) => jsonBody)
+}
+fetchToDo(url1)
+        .then((body) => {
+            console.log(body)
+        })
+
+//Напишіть функцію яке буде робити запит до серверу за цим ендпоінтом
+//https://jsonplaceholder.typicode.com/users/1
+// Функція повинна повертати як результат Promise що повертає об’єкт user
+
+const url2 = "https://jsonplaceholder.typicode.com/users/1"
+function fetchUser(url2) {
+    return fetch(url2)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody)
+}
+fetchUser(url2)
+    .then((user) => {
+        console.log(user)
+    })
+
+
+//Викорситайте методи Promise.all та Promise.race передавши їм як аргумент
+// масив з викликами функцій з п.п. 1-2.
+// Присвойте значення отримані від цих виразів до змінних
+// Функція для здійснення запиту до серверу та повернення результату як Promise
+
+Promise.all([fetchToDo(url1), fetchUser(url2)])
+    .then((results) => {
+        const todo = results[0]
+        const user = results[1]
+        console.log("Result todo:", todo)
+        console.log("Result user:", user)
+    })
+
+// Promise.race
+Promise.race([fetchToDo(url1), fetchUser(url2)])
+    .then((result) => {
+        console.log("My first result:", result)
+    })
+    .catch((error) => {
+        console.error("Упс, сталась помилка:", error)
+     })
+
